50c0b81d202cd548448afb6d51e5a6ff6104f574
Kaim Lee <hslee2000@gmail.com>
2017-08-05 23:56:53 +0700

    under construction

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 let router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'poloLender Pro' });
+  res.render('index', { title: '폴로랜더 프로 - 한글버전' });
 });
 
 module.exports.router= router;
